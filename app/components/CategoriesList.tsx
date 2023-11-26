4c2cdb9590861f3f831c0feb9351324e3a2c083f
praneethrms04 <praneethrms04@gmail.com>
2023-11-26 22:40:52 +0530

    categories-list ui

--- app/components/CategoriesList.tsx
+++ app/components/CategoriesList.tsx
@@ -1,12 +1,18 @@
 "use client"
 import React from 'react'
+import Link from 'next/link'
+import { categoriesData } from '../utils'
 
 const CategoriesList = () => {
   return (
-    <div>
-      Categories List
+    <div className="flex gap-2 text-sm flex-wrap">
+      {
+         categoriesData && categoriesData.map((category)=>(
+            <Link href={`/categories/${category.name}`} className=' bg-gradient-to-r from-teal-300 via-blue-300 to-blue-400 px-4 py-2 rounded-md  font-semibold '>{category.name} </Link>
+         ))
+      }
     </div>
   )
 }
-
+ 
 export default CategoriesList
